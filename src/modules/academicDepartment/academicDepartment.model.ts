68fdbda96265d9f7150f6efe1ce81683af1ed6f1
dipujafar <dipujafarmr@gmail.com>
2026-01-04 12:26:38 +0600

    made sucure the platform

--- src/modules/academicDepartment/academicDepartment.model.ts
+++ src/modules/academicDepartment/academicDepartment.model.ts
@@ -19,6 +19,28 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   },
 );
 
+academicDepartmentSchema.pre('save', async function (next) {
+  const isDepartmentExit = await AcademicDepartment.findOne({
+    name: this.name,
+  });
+
+  if (isDepartmentExit) {
+    throw new Error('Department is already exist');
+  }
+  next();
+});
+
+academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery();
+  const isDepartmentExit = await AcademicDepartment.findOne(query);
+
+  if (!isDepartmentExit) {
+    throw new Error('This Department does not exist');
+  }
+
+  next();
+});
+
 export const AcademicDepartment = model<TAcademicDepartment>(
   'AcademicDepartment',
   academicDepartmentSchema,
--- src/modules/academicDepartment/academicDepartment.services.ts
+++ src/modules/academicDepartment/academicDepartment.services.ts
@@ -1,9 +1,6 @@
 import { TAcademicDepartment } from './academicDepartment.interface';
 import { AcademicDepartment } from './academicDepartment.model';
 
-import { PDFDocument, rgb } from 'pdf-lib';
-import { readFile, writeFile } from 'fs/promises';
-
 const createAcademicDepartmentIntoDB = async (payload: TAcademicDepartment) => {
   const result = await AcademicDepartment.create(payload);
   return result;
@@ -37,33 +34,6 @@ const updateAcademicDepartmentIntoDB = async (
 
 
 
-
-const generateNewPDF = async () => {
-  const pdfBytes = await readFile('templete.pdf');
-  const pdfDoc = await PDFDocument.load(pdfBytes);
-
-  const page = pdfDoc.getPages()[0];
-
-  // 1. Draw white rectangle (cover old text)
-  page.drawRectangle({
-    x: 425, // from left
-    y: 730, // from bottom
-    width: 100,
-    height: 20,
-    color: rgb(1, 1, 1), // WHITE
-  });
-
-  // 2. Draw new text
-  page.drawText('13 july 2025', {
-    x: 430,
-    y: 735,
-    size: 12,
-    color: rgb(0, 0, 0),
-  });
-
-  const updatedPdf = await pdfDoc.save();
-  await writeFile('output.pdf', updatedPdf);
-};
 
 
 export const AcademicDepartmentServices = {
